f382ad24360e2d400419c7871f295ea3da6000fa
MayCabetti <mayaracabetti@gmail.com>
2026-04-01 09:31:18 -0300

    Multiplos de 5

---
+++ ex05/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Insira um número:"));
+
+if(numero % 5 === 0) {
+    alert("Esse número é multiplo de 5");
+} else {
+        alert ("Esse número NÃO é multiplo de 5");
+    }
