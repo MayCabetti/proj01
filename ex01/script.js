fe5b7113d838c1ef4e3b6371ed21c7caebfa1f5b
MayCabetti <mayaracabetti@gmail.com>
2026-04-01 08:49:14 -0300

    N° Positivo/Negativo

--- ex01/script.js
+++ ex01/script.js
@@ -2,6 +2,8 @@ let numero = Number(prompt("Insira um número:"));
 
 if(numero > 0) {
     alert("Número positivo");
+    } else if(numero === 0) {
+        alert ("Número neltro");
     } else {
         alert ("Número negativo");
     }
